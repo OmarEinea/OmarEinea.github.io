1103aeab1b5a8f0a757196acae6e8253519831ab
Omar Einea <eineao@gmail.com>
2018-05-27 15:21:17 +0300

    Base: added webpack configurations file

---
+++ webpack.config.js
@@ -0,0 +1,25 @@
+var webpack = require('webpack');
+
+module.exports = {
+	entry: './app/App.js',
+	output: {
+		path: __dirname,
+		filename: 'app.min.js'
+	},
+	module: {
+		rules: [
+			{
+				test: /\.js$/,
+				loader: 'babel-loader',
+				exclude: /node_modules/,
+				query: {
+					presets: ['env', 'react']
+				}
+			}
+		]
+	},
+	externals: {
+		'react': 'React',
+		'react-dom': 'ReactDOM'
+	}
+};
