2422066de597f1c8edfced3618c929b3661066ff
Omar Einea <eineao@gmail.com>
2018-06-03 22:46:46 +0300

    App: added application navigation bar with pages titles

--- app/App.js
+++ app/App.js
@@ -1,10 +1,24 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { AppBar, Toolbar, Grid, Button } from 'material-ui';
+import './App.css';
 
 class App extends React.Component {
   render() {
     return (
-      <div>A website about Omar Einea</div>
+      <Grid container style={{maxWidth: 1000, margin: '-8px auto'}}>
+        <AppBar id="app-bar" position="static" elevation="0">
+          <Toolbar>
+            <div style={{flex: 'auto'}}/>
+            <Button>Skills</Button>
+            <Button>Projects</Button>
+            <Button>Certificates</Button>
+            <Button>Events</Button>
+            <Button>Courses</Button>
+            <Button>Profiles</Button>
+          </Toolbar>
+        </AppBar>
+      </Grid>
     );
   }
 }
