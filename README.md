5978595c1ee512ccf2113f4132098d305bfe4261
Omar Einea <eineao@gmail.com>
2018-06-04 19:55:02 +0300

    Base: replaced icons & font with Font Awesome and Quicksand

--- README.md
+++ README.md
@@ -1,6 +1,6 @@
 #  Omar Einea | Website
 
-A website about myself, made using ReactJS. Its data is dynamically fetched from Firebase when it loads.
+A website about myself, made using ReactJS. Its data is dynamically fetched from Firebase when it loads.  
 It will include details about my projects, skills, certifications, profiles, courses and events I attended or organized.
 
 This website can be accessed through one of the following links:
@@ -13,6 +13,8 @@ This website can be accessed through one of the following links:
 
 - [ReactJS](https://github.com/facebook/react)
 - [Material UI](https://github.com/mui-org/material-ui)
+- [Font Awesome](https://github.com/FortAwesome/Font-Awesome)
+- [Quicksand Font](https://fonts.google.com/specimen/Quicksand)
 - [FetchJS](https://github.com/github/fetch)
 - [WebpackJS](https://github.com/webpack/webpack)
 - [BabelJS](https://github.com/babel/babel)
--- app/App.js
+++ app/App.js
@@ -1,26 +1,28 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import { AppBar, Toolbar, Grid, Button } from 'material-ui';
+import { MuiThemeProvider, createMuiTheme, AppBar, Toolbar, Grid, Button } from 'material-ui';
 import Home from './home/Home';
 import './App.css';
 
 class App extends React.Component {
   render() {
     return (
-      <Grid container style={{maxWidth: 1000, margin: '-8px auto'}}>
-        <AppBar id="app-bar" position="static" elevation="0">
-          <Toolbar>
-            <div style={{flex: 'auto'}}/>
-            <Button>Skills</Button>
-            <Button>Projects</Button>
-            <Button>Certificates</Button>
-            <Button>Events</Button>
-            <Button>Courses</Button>
-            <Button>Profiles</Button>
-          </Toolbar>
-        </AppBar>
-        <Home/>
-      </Grid>
+      <MuiThemeProvider theme={createMuiTheme({typography: {fontFamily: 'Quicksand'}})}>
+        <Grid container style={{maxWidth: 1000, margin: '-8px auto'}}>
+          <AppBar id="app-bar" position="static" elevation="0">
+            <Toolbar>
+              <div style={{flex: 'auto'}}/>
+              <Button>Skills</Button>
+              <Button>Projects</Button>
+              <Button>Certificates</Button>
+              <Button>Events</Button>
+              <Button>Courses</Button>
+              <Button>Profiles</Button>
+            </Toolbar>
+          </AppBar>
+          <Home/>
+        </Grid>
+      </MuiThemeProvider>
     );
   }
 }
