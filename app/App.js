e67306abd302083a75f55033f3cccdb319acb8b8
Omar Einea <eineao@gmail.com>
2018-06-04 20:10:23 +0300

    App: colored navigation buttons from orange to purple

--- app/App.js
+++ app/App.js
@@ -9,15 +9,15 @@ class App extends React.Component {
     return (
       <MuiThemeProvider theme={createMuiTheme({typography: {fontFamily: 'Quicksand'}})}>
         <Grid container style={{maxWidth: 1000, margin: '-8px auto'}}>
-          <AppBar id="app-bar" position="static" elevation="0">
+          <AppBar position="static" elevation="0">
             <Toolbar>
               <div style={{flex: 'auto'}}/>
-              <Button>Skills</Button>
-              <Button>Projects</Button>
-              <Button>Certificates</Button>
-              <Button>Events</Button>
-              <Button>Courses</Button>
-              <Button>Profiles</Button>
+              <Button style={{color: '#9E125E'}}>Skills</Button>
+              <Button style={{color: '#DB236B'}}>Projects</Button>
+              <Button style={{color: '#E32f4C'}}>Certificates</Button>
+              <Button style={{color: '#F24354'}}>Events</Button>
+              <Button style={{color: '#FA5E35'}}>Courses</Button>
+              <Button style={{color: '#FE7131'}}>Profiles</Button>
             </Toolbar>
           </AppBar>
           <Home/>
