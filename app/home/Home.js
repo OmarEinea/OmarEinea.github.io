cc83cc9336c256e8062e8e9ac65f460b749d0a86
Omar Einea <eineao@gmail.com>
2018-06-13 11:29:01 +0300

    App: added some gerneral CSS & linked Profiles in Home
    Also:
    - Ignored db/ folder instead of just bio.html
    - Encode slashes in url function of db.js

--- app/home/Home.js
+++ app/home/Home.js
@@ -1,12 +1,14 @@
 import { Component } from 'react';
 import { Grid } from 'material-ui';
 import Intro from './intro/Intro';
+import Profiles from './profiles/Profiles';
 
 export default class Home extends Component {
   render() {
     return (
       <Grid container>
         <Intro/>
+        <Profiles/>
       </Grid>
     );
   }
