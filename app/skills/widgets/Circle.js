8014426bd9e3b681f2051ba84bbb219d2485a803
Omar Einea <eineao@gmail.com>
2018-06-19 17:08:29 +0300

    Skills: added a circle widget with skill progress and logo

---
+++ app/skills/widgets/Circle.js
@@ -0,0 +1,22 @@
+import { Component } from 'react';
+import { Grid, Typography, CircularProgress } from 'material-ui';
+import { logo } from '../../db';
+
+export default class Circle extends Component {
+  render() {
+    const [ name, rate ] = this.props.skill;
+    return (
+      <Grid item direction="column" align="center" style={{margin: 16}}>
+        <div style={{position: 'relative', marginBottom: 8}}>
+          <CircularProgress variant="static" value={100} thickness={7} size={150}
+            style={{position: 'absolute', left: 0, top: 0, color: '#EEE'}}/>
+          <CircularProgress variant="static" value={rate} thickness={7} size={150}/>
+          <img width="60" src={logo(name)} style={{position: 'absolute', left: 45, top: 45}}/>
+        </div>
+        <Typography variant="title" style={{color: '#424242'}}>
+          {name}
+        </Typography>
+      </Grid>
+    );
+  }
+}
