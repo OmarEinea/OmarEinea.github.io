b8f6752d07ae2b7fbaed9366823bb3a04c8683d1
Omar Einea <eineao@gmail.com>
2018-06-22 18:34:45 +0300

    Home: used skills lines and completed skills section

--- app/home/sections/Skills.js
+++ app/home/sections/Skills.js
@@ -1,23 +1,35 @@
 import { Component } from 'react';
-import { Grid } from 'material-ui';
+import { Grid, Typography } from 'material-ui';
 import { get } from '../../db';
 import Circle from '../../skills/widgets/Circle';
+import Line from '../../skills/widgets/Line';
 
 export default class Skills extends Component {
   constructor() {
     super();
     this.state = {circles: [], lines: []};
     get('skills/top').then(data => data.json()).then(skills => this.setState({
-      circles: Object.entries(skills.circles).reverse()
+      circles: Object.entries(skills.circles).reverse(),
+      lines: Object.entries(skills.lines).reverse()
     }));
   }
 
   render() {
     return (
       <Grid container style={{backgroundColor: '#FAFAFA'}}>
-        <Grid container class="container">
-          <Grid container justify="center" md={7} class="box">
-            {this.state.circles.map(skill => <Circle skill={skill}/>)}
+        <Grid container class="container" style={{paddingTop: 16, paddingBottom: 24}}>
+          <Grid container justify="center">
+            <Typography variant="display1" style={{padding: 28}}>My Skills</Typography>
+          </Grid>
+          <Grid container md={7} xs={12} style={{paddingRight: 12}} class="no-sm-padding">
+            <Grid container justify="center" class="box">
+              {this.state.circles.map(skill => <Circle skill={skill}/>)}
+            </Grid>
+          </Grid>
+          <Grid container md={5} xs={12} style={{paddingLeft: 12}} class="no-sm-padding">
+            <Grid container direction="column" class="box">
+              {this.state.lines.map(skill => <Line skill={skill}/>)}
+            </Grid>
           </Grid>
         </Grid>
       </Grid>
