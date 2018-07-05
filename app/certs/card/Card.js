081b788da0dafd13bf97dc9996ab3c7b9ba63525
Omar Einea <eineao@gmail.com>
2018-07-05 21:45:23 +0300

    Certificates: added styles file & open card image on click

--- app/certs/card/Card.js
+++ app/certs/card/Card.js
@@ -1,20 +1,24 @@
 import { Component } from 'react';
-import { ClickAwayListener, Typography, Collapse } from 'material-ui';
+import { ClickAwayListener, Typography, Collapse, Modal } from 'material-ui';
 import { Card, CardMedia, CardContent, CardActions, IconButton } from 'material-ui';
 import { url } from '../../db';
+import './Card.css';
 
 export default class CertCard extends Component {
-  state = {expanded: false};
+  state = {text: false, image: false};
 
   render() {
-    const [ title, { desc, auth } ] = this.props.cert, { expanded } = this.state;
+    const { text, image } = this.state,
+      [ title, { desc, auth } ] = this.props.cert;
     return (
       <Card style={{margin: '6px 8px', position: 'relative'}}>
-        <CardMedia style={{paddingTop: '70%', marginBottom: 82}}
-          image={url(`certs/${title}.jpg`)}/>
-        <ClickAwayListener onClickAway={() => {if(expanded) this.setState({expanded: false})}}>
-          <Collapse in={expanded} collapsedHeight="82px" timeout="auto"
-            style={{position: 'absolute', bottom: 0, backgroundColor: 'white', maxHeight: '100%', width: '100%'}}>
+        <CardMedia style={{paddingTop: '70%', marginBottom: 82, cursor: 'pointer'}}
+          image={url(`certs/small/${title}.jpg`)} onClick={() => this.setState({image: true})}/>
+        <Modal open={image} onBackdropClick={() => this.setState({image: false})}>
+          <img class="modal-image" src={url(`certs/${title}.jpg`)}/>
+        </Modal>
+        <ClickAwayListener onClickAway={() => {if(text) this.setState({text: false})}}>
+          <Collapse in={text} collapsedHeight="82px" timeout="auto" class="collapse">
             <CardContent style={{padding: 16}}>
               <Typography variant="title" style={{fontSize: 19, marginBottom: 8}} noWrap>
                 {title}
@@ -23,9 +27,9 @@ export default class CertCard extends Component {
                 <Typography style={{flex: 1, color: '#616161'}} noWrap>
                   <i class="fas fa-university" style={{marginRight: 4}}/> {auth}
                 </Typography>
-                <IconButton onClick={() => this.setState({expanded: !expanded})}
+                <IconButton onClick={() => this.setState({text: !text})}
                   style={{margin: '-8px -10px -12px', width: 40, height: 40}}>
-                  <i style={{fontSize: 12}} class={'fas fa-chevron-' + (expanded ? 'down' : 'up')}/>
+                  <i style={{fontSize: 12}} class={'fas fa-chevron-' + (text ? 'down' : 'up')}/>
                 </IconButton>
               </CardActions>
             </CardContent>
--- app/db.js
+++ app/db.js
@@ -2,6 +2,6 @@ import fetch from 'fetch';
 
 export const json = data => data.json();
 export const get = query => fetch(`https://eineao-website.firebaseio.com/${query}.json`).then(json);
-export const url = image => `https://firebasestorage.googleapis.com/v0/b/eineao-website.appspot.com/o/${image.replace('/', '%2F')}?alt=media`;
+export const url = image => `https://firebasestorage.googleapis.com/v0/b/eineao-website.appspot.com/o/${image.replace(/\//g, '%2F')}?alt=media`;
 export const logo = name => url(`logos/${name.replace(' ', '%20')}.png`);
 export default { get, url, logo, json };
