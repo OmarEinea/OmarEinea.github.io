5f3fe867c2f44b8066bd2d280c6d8538b6be75b8
Omar Einea <eineao@gmail.com>
2018-07-11 23:41:51 +0600

    Base: replaced all tabs in Webpeck's configs file with spaces

--- webpack.config.js
+++ webpack.config.js
@@ -1,47 +1,47 @@
 module.exports = (_, { mode }) => {
-	const configs = {
-		entry: './app/App.js',
-		output: {
-			path: __dirname,
-			filename: 'app.min.js'
-		},
-		module: {
-			rules: [
-				{
-					test: /\.js$/,
-					loader: 'babel-loader',
-					exclude: /node_modules/,
-					query: {
-						presets: ['env', 'react'],
-						plugins: ['react-html-attrs', 'transform-class-properties']
-					}
-				}, {
-					test: /\.css$/,
-					exclude: /node_modules/,
-					use: ['style-loader', 'css-loader?url=false&minimize']
-				}
-			]
-		}
-	};
-	if(mode === 'production')
-		configs.externals = {
-			'react': 'React',
-			'react-dom': 'ReactDOM',
-			'material-ui': 'window["material-ui"]',
-			'fetch': 'fetch'
-		};
-	else {
-		configs.resolve = {
-			alias: {
-				'material-ui': '@material-ui/core',
-				'fetch': 'whatwg-fetch'
-			}
-		};
-		configs.plugins = [
-			new (require('webpack')).ProvidePlugin({
-				'React': 'react'
-			})
-		];
-	}
-	return configs;
+  const configs = {
+    entry: './app/App.js',
+    output: {
+      path: __dirname,
+      filename: 'app.min.js'
+    },
+    module: {
+      rules: [
+        {
+          test: /\.js$/,
+          loader: 'babel-loader',
+          exclude: /node_modules/,
+          query: {
+            presets: ['env', 'react'],
+            plugins: ['react-html-attrs', 'transform-class-properties']
+          }
+        }, {
+          test: /\.css$/,
+          exclude: /node_modules/,
+          use: ['style-loader', 'css-loader?url=false&minimize']
+        }
+      ]
+    }
+  };
+  if(mode === 'production')
+    configs.externals = {
+      'react': 'React',
+      'react-dom': 'ReactDOM',
+      'material-ui': 'window["material-ui"]',
+      'fetch': 'fetch'
+    };
+  else {
+    configs.resolve = {
+      alias: {
+        'material-ui': '@material-ui/core',
+        'fetch': 'whatwg-fetch'
+      }
+    };
+    configs.plugins = [
+      new (require('webpack')).ProvidePlugin({
+        'React': 'react'
+      })
+    ];
+  }
+  return configs;
 };
