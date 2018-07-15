14178e0b1230e3a81e9292e698c999a16d16abf1
Omar Einea <eineao@gmail.com>
2018-07-15 17:46:26 +0600

    Utils: prepared Gallery for events by adding buttons & logic

--- app/utils/gallery/Gallery.js
+++ app/utils/gallery/Gallery.js
@@ -1,31 +1,56 @@
 import { Component } from 'react';
-import { ClickAwayListener, Typography, Modal, Grid } from 'material-ui';
+import { Typography, Modal } from 'material-ui';
 import { url } from 'db';
 import './Gallery.css';
 
 export default class Gallery extends Component {
-  state = {loaded: false};
+  state = {loaded: false, index: 0};
+
+  componentWillMount() {
+    let { title, images, folder } = this.props;
+    this.urls = [];
+    if(images)
+      for(let i = 1; i <= images.length; i++)
+        this.urls.push(`${folder}/${title}/${i}.jpg`);
+    else {
+      this.urls.push(`${folder}/${title}.jpg`);
+      images = [title];
+    }
+    this.images = images;
+  }
+
+  gotoImage(index) {
+    return () => this.setState({index, loaded: false});
+  }
 
   render() {
-    const { images, isOpen, onClose } = this.props;
+    const { isOpen, onClose } = this.props, { loaded, index } = this.state;
     return (
-      <Modal open={isOpen} class="gallery">
-        <Grid container justify="center" class="container">
-          <ClickAwayListener onClickAway={onClose}>
-            <div class="white-text">
-              <Typography variant="subheading" style={{margin: '16px 0 8px', height: '24px'}}>
-                {this.state.loaded && <span>
-                  {images[0]}
-                  <i class="fas fa-times" onClick={onClose}
-                    style={{lineHeight: '24px', float: 'right', cursor: 'pointer'}}/>
-                </span>}
-              </Typography>
-              <img src={url(`certs/${images[0]}.jpg`)}
-                style={{maxHeight: 'calc(100vh - 68px)', margin: 'auto'}}
-                onLoad={() => this.setState({loaded: true})}/>
+      <Modal open={isOpen} onBackdropClick={onClose} class="gallery">
+        <div class="content white-text">
+          <div>
+            <Typography variant="subheading" style={{height: 28}}>
+              {loaded && <span>
+                {this.images[index]}
+                <i class="fas fa-times" onClick={onClose}
+                  style={{float: 'right', marginRight: -5}}/>
+              </span>}
+            </Typography>
+            <img src={url(this.urls[index])}
+              style={{maxHeight: 'calc(100vh - 68px)', maxWidth: '100%'}}
+              onLoad={() => this.setState({loaded: true})}/>
+          </div>
+          {index > 0 &&
+            <div class="nav" style={{left: 0}}>
+              <i onClick={this.gotoImage(index - 1)} class="fas fa-chevron-left"/>
             </div>
-          </ClickAwayListener>
-        </Grid>
+          }
+          {this.images.length > 1 && index + 1 < this.images.length &&
+            <div class="nav" style={{right: 0}}>
+              <i onClick={this.gotoImage(index + 1)} class="fas fa-chevron-right"/>
+            </div>
+          }
+        </div>
       </Modal>
     );
   }
