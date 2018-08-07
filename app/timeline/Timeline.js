446944c9a80665ab338b60cb2b645e793aebfbf0
Omar Einea <eineao@gmail.com>
2018-08-07 17:58:34 +0300

    Timeline: reversed year & added institutes & cleaned up CSS

--- app/timeline/Timeline.js
+++ app/timeline/Timeline.js
@@ -10,10 +10,11 @@ export default class Timeline extends Component {
     const structured = {};
     Object.entries(items).map(([range, desc]) => {
       const [ a, b ] = range.split(',');
-      structured[a] = [(b || a) - a, desc];
+      structured[a] = [desc.split(';'), (b || a) - a];
     });
     return structured;
   }
+
   componentWillMount() {
     get('timeline').then(({ years, cities, institutes }) => {
       this.setState({
@@ -25,26 +26,40 @@ export default class Timeline extends Component {
   }
 
   render() {
-    const { years, cities, institutes } = this.state;
-    let cityStart, cityCount, instStart, instCount;
+    const { years, cities, institutes } = this.state,
+      is = (string, type) => string.includes(type) && type.toLowerCase();
+    let city, cityStart, cityCount, institute, instDesc, instStart, instCount;
     return (
-      <Grid container direction="column" alignItems="center"
-        class="container" style={{margin: '24px auto'}}>
+      <Grid container direction="column-reverse" alignItems="center"
+        class="container" style={{margin: '36px auto'}}>
         {years.map(([year, state]) => {
-          if(cityStart = year in cities) cityCount = cities[year][0];
-          if(instStart = year in institutes) instCount = institutes[year][0];
+          if(cityStart = year in cities)
+            [ city, cityCount ] = cities[year];
+          if(instStart = year in institutes)
+            [[ institute, instDesc], instCount ] = institutes[year];
           return (
             <div class={'year' + (cityStart && ' start' || '') + (cityCount-- === 0 && ' end' || '')}>
               {cityStart && <span>
-                <Typography class="number"><i/>{+year + 1994}</Typography>
                 <i class="city-vline"><i/></i>
+                <Typography class="number">{+year + 1994}</Typography>
                 <Typography class="city" variant="subheading" noWrap>
-                  <i class="fas fa-map-marker-alt"/> {cities[year][1]}
+                  <i class="fas fa-map-marker-alt"/> {city[0]}
                 </Typography>
               </span>}
-              {+year === years.length && <Typography class="last number"><i/>{+year + 1995}</Typography>}
-              {instCount >= 0 && <i class={'institute' + (instStart && ' start' || '') + (instCount-- === 0 && ' end' || '')}/>}
-              <Typography align="center" class="state">{state || <i class="fas fa-child fa-lg"/>}</Typography>
+              {instCount === 0 && <span class="inst-box white-text"><i/>
+                <Typography variant="subheading" style={instDesc && {marginBottom: 6}} noWrap>
+                  <i style={{marginRight: 8}}
+                    class={'fas fa-' + (is(institute, 'School') || is(institute, 'University') || '')}/>
+                  <b>{institute}</b>
+                </Typography>
+                <Typography variant="caption">{instDesc}</Typography>
+              </span>}
+              {instCount >= 0 &&
+                <i class={'inst-sign' + (instStart && ' start' || '') + (instCount-- === 0 && ' end' || '')}/>
+              }
+              <Typography align="center" style={{flex: 1, color: '#616161'}}>
+                {state || <i class="fas fa-child fa-lg"/>}
+              </Typography>
             </div>
           );
         })}
