611c799a150c4c9b705a45d7e04d46f728b766b9
Omar Einea <eineao@gmail.com>
2018-08-08 23:11:26 +0300

    Timeline: added checkboxes to toggle three kinds of bubbles

--- app/timeline/Timeline.js
+++ app/timeline/Timeline.js
@@ -1,10 +1,10 @@
 import { Component } from 'react';
-import { Grid, Typography } from 'material-ui';
+import { Grid, Typography, FormControlLabel, Checkbox } from 'material-ui';
 import { get } from 'db';
 import './Timeline.css';
 
 export default class Timeline extends Component {
-  state = {years: []};
+  state = {years: [], showEvents: true, showCities: true, showInsts: true};
 
   structureItems(items) {
     const structured = {};
@@ -25,11 +25,11 @@ export default class Timeline extends Component {
   }
 
   render() {
-    const { years, cities, institutes, events } = this.state,
+    const { years, cities, institutes, events, showEvents, showCities, showInsts } = this.state,
       is = (string, type) => string.includes(type) && type.toLowerCase();
     let city, cityStart, cityCount, institute, instDesc, instStart, instCount;
     return (
-      <Grid container direction="column-reverse" alignItems="center"
+      <Grid container direction="column-reverse" alignItems="flex-start"
         class="container" style={{margin: '36px auto'}}>
         {years.map(([year, state]) => {
           if(cityStart = year in cities)
@@ -38,25 +38,27 @@ export default class Timeline extends Component {
             [[ institute, instDesc], instCount ] = institutes[year];
           return (
             <div class={'year' + (cityStart && ' start' || '') + (cityCount-- === 0 && ' end' || '')}>
-              {cityStart && <span>
+              {showCities && cityStart && <span>
                 <i class="city-vline"><i/></i>
                 <Typography class="number">{+year + 1994}</Typography>
                 <Typography class="city" variant="subheading" noWrap>
                   <i class="fas fa-map-marker-alt"/> {city[0]}
                 </Typography>
               </span>}
-              {instCount === 0 && <span class="inst-box white-text"><i/>
-                <Typography variant="subheading" style={instDesc && {marginBottom: 6}} noWrap>
-                  <i style={{marginRight: 8}}
-                    class={'fas fa-' + (is(institute, 'School') || is(institute, 'University') || '')}/>
-                  <b>{institute}</b>
-                </Typography>
-                <Typography variant="caption">{instDesc}</Typography>
+              {showInsts && <span>
+                {instCount === 0 && <span class="inst-box white-text"><i/>
+                  <Typography variant="subheading" style={instDesc && {marginBottom: 6}} noWrap>
+                    <i style={{marginRight: 8}}
+                      class={'fas fa-' + (is(institute, 'School') || is(institute, 'University') || '')}/>
+                    <b>{institute}</b>
+                  </Typography>
+                  <Typography variant="caption">{instDesc}</Typography>
+                </span>}
+                {instCount >= 0 &&
+                  <i class={'inst-sign' + (instStart ? ' start' : '') + (instCount-- === 0 ? ' end' : '')}/>
+                }
               </span>}
-              {instCount >= 0 &&
-                <i class={'inst-sign' + (instStart ? ' start' : '') + (instCount-- === 0 ? ' end' : '')}/>
-              }
-              {year in events &&
+              {showEvents && year in events &&
                 <Typography class={'white-text event' + (year - 1 in events ? ' up': '')}>
                   <i/>{events[year]}
                 </Typography>
@@ -67,6 +69,26 @@ export default class Timeline extends Component {
             </div>
           );
         })}
+        <Grid container justify="space-evenly" class="ctrls">
+          <FormControlLabel class="orange" label="Events" control={
+            <Checkbox
+              checked={showEvents}
+              onChange={() => {this.setState({showEvents: !showEvents})}}
+            />
+          }/>
+          <FormControlLabel class="white" label="Cities" control={
+            <Checkbox
+              checked={showCities}
+              onChange={() => {this.setState({showCities: !showCities})}}
+            />
+          }/>
+          <FormControlLabel class="black" label="Institutes" control={
+            <Checkbox
+              checked={showInsts}
+              onChange={() => {this.setState({showInsts: !showInsts})}}
+            />
+          }/>
+        </Grid>
       </Grid>
     );
   }
