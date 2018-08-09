c89f9f4570384c40ab823312f7e8c0726a8aeab4
Omar Einea <eineao@gmail.com>
2018-08-09 18:12:24 +0300

    Timeline: supported smaller screens by hiding and aligning

--- app/timeline/Timeline.js
+++ app/timeline/Timeline.js
@@ -16,6 +16,10 @@ export default class Timeline extends Component {
   }
 
   componentWillMount() {
+    (this.onResize = () => {
+      const pc = window.innerWidth > 650;
+      this.setState(prev => ({pc, showInsts: prev.pc === pc ? prev.showInsts : pc}));
+    })();
     get('timeline').then(({ years, cities, institutes, events }) => this.setState({
       years: Object.entries(years).slice(1),
       cities: this.structureItems(cities),
@@ -25,12 +29,13 @@ export default class Timeline extends Component {
   }
 
   render() {
-    const { years, cities, institutes, events, showEvents, showCities, showInsts } = this.state,
+    const { years, cities, institutes, events, showEvents, showCities, showInsts, pc } = this.state,
       is = (string, type) => string.includes(type) && type.toLowerCase();
     let city, cityStart, cityCount, institute, instDesc, instStart, instCount;
     return (
-      <Grid container direction="column-reverse" alignItems="flex-start"
-        class="container" style={{margin: '36px auto'}}>
+      <Grid container direction="column-reverse"
+        alignItems={pc ? 'center': showInsts ? 'flex-start' : 'flex-end'}
+        class="container" style={{margin: '36px auto', overflow: 'hidden'}}>
         {years.map(([year, state]) => {
           if(cityStart = year in cities)
             [ city, cityCount ] = cities[year];
@@ -39,8 +44,9 @@ export default class Timeline extends Component {
           return (
             <div class={'year' + (cityStart && ' start' || '') + (cityCount-- === 0 && ' end' || '')}>
               {showCities && cityStart && <span>
-                <i class="city-vline"><i/></i>
-                <Typography class="number">{+year + 1994}</Typography>
+                <b class="city-vline"><i/>
+                  <Typography class="number">{+year + 1994}</Typography>
+                </b>
                 <Typography class="city" variant="subheading" noWrap>
                   <i class="fas fa-map-marker-alt"/> {city[0]}
                 </Typography>
@@ -69,27 +75,39 @@ export default class Timeline extends Component {
             </div>
           );
         })}
-        <Grid container justify="space-evenly" class="ctrls">
+        <Grid container justify="space-evenly" class={'ctrls' + (pc ? '' : ' margin')}>
           <FormControlLabel class="orange" label="Events" control={
             <Checkbox
               checked={showEvents}
-              onChange={() => {this.setState({showEvents: !showEvents})}}
+              onChange={() => {this.setState({showEvents: !showEvents, showInsts: !pc ? false : showInsts})}}
             />
           }/>
           <FormControlLabel class="white" label="Cities" control={
             <Checkbox
               checked={showCities}
-              onChange={() => {this.setState({showCities: !showCities})}}
+              onChange={() => {this.setState({showCities: !showCities, showInsts: !pc ? false : showInsts})}}
             />
           }/>
           <FormControlLabel class="black" label="Institutes" control={
             <Checkbox
               checked={showInsts}
-              onChange={() => {this.setState({showInsts: !showInsts})}}
+              onChange={() => {this.setState({
+                showInsts: !showInsts,
+                showCities: !pc ? false : showCities,
+                showEvents: !pc ? false : showEvents
+              })}}
             />
           }/>
         </Grid>
       </Grid>
     );
+  }
+
+  componentDidMount() {
+    window.addEventListener('resize', this.onResize);
+  }
+
+  componentWillUnmount() {
+    window.removeEventListener('resize', this.onResize);
   }
 }
