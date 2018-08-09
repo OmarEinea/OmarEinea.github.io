994932aa8aabe5b0145073f8de180d48d71b945d
Omar Einea <eineao@gmail.com>
2018-08-09 21:34:33 +0300

    Timeline: made radio buttons only on mobile & added tooltips

--- app/timeline/Timeline.js
+++ app/timeline/Timeline.js
@@ -1,10 +1,10 @@
 import { Component } from 'react';
-import { Grid, Typography, FormControlLabel, Checkbox } from 'material-ui';
+import { Grid, Typography, FormControlLabel, RadioGroup, Radio, Tooltip } from 'material-ui';
 import { get } from 'db';
 import './Timeline.css';
 
 export default class Timeline extends Component {
-  state = {years: [], showEvents: true, showCities: true, showInsts: true};
+  state = {years: [], showEvents: true};
 
   structureItems(items) {
     const structured = {};
@@ -16,10 +16,7 @@ export default class Timeline extends Component {
   }
 
   componentWillMount() {
-    (this.onResize = () => {
-      const pc = window.innerWidth > 650;
-      this.setState(prev => ({pc, showInsts: prev.pc === pc ? prev.showInsts : pc}));
-    })();
+    (this.onResize = () => this.setState({pc: window.innerWidth > 650}))();
     get('timeline').then(({ years, cities, institutes, events }) => this.setState({
       years: Object.entries(years).slice(1),
       cities: this.structureItems(cities),
@@ -29,21 +26,21 @@ export default class Timeline extends Component {
   }
 
   render() {
-    const { years, cities, institutes, events, showEvents, showCities, showInsts, pc } = this.state,
+    const { years, cities, institutes, events, showEvents, pc } = this.state,
       is = (string, type) => string.includes(type) && type.toLowerCase();
     let city, cityStart, cityCount, institute, instDesc, instStart, instCount;
     return (
       <Grid container direction="column-reverse"
-        alignItems={pc ? 'center': showInsts ? 'flex-start' : 'flex-end'}
-        class="container" style={{margin: '36px auto', overflow: 'hidden'}}>
-        {years.map(([year, state]) => {
+        alignItems={pc ? 'center': showEvents ? 'flex-end' : 'flex-start'}
+        class="container" style={{margin: '48px auto 36px', overflow: 'hidden'}}>
+        {years.map(([year, state], index) => {
           if(cityStart = year in cities)
             [ city, cityCount ] = cities[year];
           if(instStart = year in institutes)
             [[ institute, instDesc], instCount ] = institutes[year];
           return (
             <div class={'year' + (cityStart && ' start' || '') + (cityCount-- === 0 && ' end' || '')}>
-              {showCities && cityStart && <span>
+              {showEvents && cityStart && <span>
                 <b class="city-vline"><i/>
                   <Typography class="number">{+year + 1994}</Typography>
                 </b>
@@ -51,7 +48,7 @@ export default class Timeline extends Component {
                   <i class="fas fa-map-marker-alt"/> {city[0]}
                 </Typography>
               </span>}
-              {showInsts && <span>
+              {(pc || !showEvents) && <span>
                 {instCount === 0 && <span class="inst-box white-text"><i/>
                   <Typography variant="subheading" style={instDesc && {marginBottom: 6}} noWrap>
                     <i style={{marginRight: 8}}
@@ -64,41 +61,24 @@ export default class Timeline extends Component {
                   <i class={'inst-sign' + (instStart ? ' start' : '') + (instCount-- === 0 ? ' end' : '')}/>
                 }
               </span>}
-              {showEvents && year in events &&
+              {(pc || showEvents) && year in events &&
                 <Typography class={'white-text event' + (year - 1 in events ? ' up': '')}>
                   <i/>{events[year]}
                 </Typography>
               }
               <Typography align="center" style={{flex: 1, color: '#616161'}}>
-                {state || <i class="fas fa-child fa-lg"/>}
+                <Tooltip title={year + ' years old' + (index === years.length - 1 ? ' (NOW)' : '')}>
+                  {state ? <span>{state}</span> : <i class="fas fa-child fa-lg"/>}
+                </Tooltip>
               </Typography>
             </div>
           );
         })}
-        <Grid container justify="space-evenly" class={'ctrls' + (pc ? '' : ' margin')}>
-          <FormControlLabel class="orange" label="Events" control={
-            <Checkbox
-              checked={showEvents}
-              onChange={() => {this.setState({showEvents: !showEvents, showInsts: !pc ? false : showInsts})}}
-            />
-          }/>
-          <FormControlLabel class="white" label="Cities" control={
-            <Checkbox
-              checked={showCities}
-              onChange={() => {this.setState({showCities: !showCities, showInsts: !pc ? false : showInsts})}}
-            />
-          }/>
-          <FormControlLabel class="black" label="Institutes" control={
-            <Checkbox
-              checked={showInsts}
-              onChange={() => {this.setState({
-                showInsts: !showInsts,
-                showCities: !pc ? false : showCities,
-                showEvents: !pc ? false : showEvents
-              })}}
-            />
-          }/>
-        </Grid>
+        {!pc && <RadioGroup value={showEvents} class="ctrls"
+          onChange={() => this.setState({showEvents: !showEvents})}>
+          <FormControlLabel value={true} control={<Radio/>} label="Events" class="orange"/>
+          <FormControlLabel value={false} control={<Radio/>} label="Phases" class="black"/>
+        </RadioGroup>}
       </Grid>
     );
   }
