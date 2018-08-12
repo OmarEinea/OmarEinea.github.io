a95394e796163fc270e9134056d2bf944561849b
Omar Einea <eineao@gmail.com>
2018-08-12 13:08:49 +0300

    Utils: added a loading component for transition between pages

---
+++ app/utils/Loading.js
@@ -0,0 +1,27 @@
+import { Component } from 'react';
+import { Grid, CircularProgress } from 'material-ui';
+import { colors } from 'db';
+
+export default class Loading extends Component {
+  state = {index: colors.length - 1};
+
+  componentWillMount() {
+    let direction = -1;
+    setInterval(() => this.setState(prev => {
+      if(prev.index + direction === colors.length)
+        direction = -1;
+      else if(prev.index + direction === 0)
+        direction = 1;
+      return {index: prev.index + direction};
+    }), 1000);
+  }
+
+  render() {
+    return (
+      <Grid container justify="center" alignItems="center"
+        style={{flex: 1, minHeight: 100, marginBottom: 16}}>
+        <CircularProgress size={64} style={{color: colors[this.state.index]}}/>
+      </Grid>
+    );
+  }
+}
