ae199b55cf9384228f1ef5146f7c4dd7d5285920
Omar Einea <eineao@gmail.com>
2019-02-22 16:28:40 +0400

    Functions: update GitHub graph & info inside updateProfiles()

--- fb/functions/index.js
+++ fb/functions/index.js
@@ -11,5 +11,22 @@ exports.updateProfiles = functions.onRequest((_, response) => {
     const { reputation, badge_counts: { gold, silver, bronze }} = data.items[0];
     database.child('stack').update({ reputation, gold, silver, bronze });
   });
+  get('https://api.github.com/users/OmarEinea').then(({followers, public_repos}) => {
+    database.child('github').update({ followers, repos: public_repos });
+  });
+  get('https://github.com/users/OmarEinea/contributions', 'text').then(html => {
+    database.child('github').update({
+      commits: Number(html.match(/<h2 class="f4.*">\s*([0-9]+)[\s\S]*<\/h2>/)[1]),
+      graph: html.match(/<svg([\s\S]*?)<\/svg>/)[0]
+        .replace('translate(16, 20)', 'translate(20, 24)')
+        .replace('height="104"', 'height="108"')
+        .replace(/dx="-14"/g, 'dx="-20"')
+        .replace(/ data-hydro-click=".*">/, '>')
+        .replace(/ data-count=".*"\/>/g, '/>')
+        .replace(/ class="(month|day|wday)"/g, '')
+        .replace(/.*style="display: none;".*\n/g, '')
+        .replace(/"/g, "'").replace(/\n */g, '')
+    });
+  });
   response.send('Updating Profiles...');
 });
