dcf95a1ae43975a6354705eff98c6654dcd9f68f
Omar Einea <eineao@gmail.com>
2019-02-23 16:39:58 +0400

    Functions: Firebase Functions block outside requests, REMOVE!

--- fb/index.js
+++ fb/index.js
@@ -1,12 +1,11 @@
-const functions = require('firebase-functions').region('europe-west1').https,
-  fetch = require('node-fetch'), json = data => data.json(), text = data => data.text(),
+const fetch = require('node-fetch'), json = data => data.json(), text = data => data.text(),
   get = (link, type) => fetch(link).then(type === 'text' ? text : json)
   database = require('firebase-admin').initializeApp({
     databaseURL: "https://eineao-website.firebaseio.com",
     apiKey: process.env.APIKEY
   }).database().ref('home/profiles');
 
-exports.updateProfiles = functions.onRequest((_, response) => {
+exports.updateProfiles = (_, response) => {
   get('https://api.stackexchange.com/2.2/users/4794459?site=stackoverflow').then(data => {
     const { reputation, badge_counts: { gold, silver, bronze }} = data.items[0];
     database.child('stack').update({ reputation, gold, silver, bronze });
@@ -29,4 +28,4 @@ exports.updateProfiles = functions.onRequest((_, response) => {
     });
   });
   response.send('Updating Profiles...');
-});
+};
