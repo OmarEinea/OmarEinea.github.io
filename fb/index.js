427c04ce07199c5a1d8b6b777ffa1bded4edf575
Omar Einea <eineao@gmail.com>
2019-02-25 12:43:31 +0400

    Functions: logged updated profiles & unified the update method

--- fb/index.js
+++ fb/index.js
@@ -1,27 +1,30 @@
-const app = require('express')(), fetch = require('node-fetch'), firebase = require('firebase-admin'), 
+const app = require('express')(), fetch = require('node-fetch'), firebase = require('firebase-admin'),
   json = data => data.json(), text = data => data.text(),
-  get = (link, type) => fetch(link).then(type === 'text' ? text : json)
+  get = (link, type) => fetch(link).then(type === 'text' ? text : json),
   database = firebase.initializeApp({
     databaseURL: 'https://eineao-website.firebaseio.com',
     credential: firebase.credential.cert({
       projectId: 'eineao-website', privateKey: process.env.KEY.replace(/\\n/g, '\n'),
       clientEmail: 'firebase-adminsdk-xxoy3@eineao-website.iam.gserviceaccount.com'
     })
-  }).database().ref('home/profiles');
+  }).database().ref('home/profiles'), update = (profile, object) => {
+    database.child(profile).update(object);
+    console.log('Updated', profile, 'with:', JSON.stringify(object).slice(0, 84));
+  };
 
 app.get('/updateProfiles', (_, response) => {
   get('https://api.stackexchange.com/2.2/users/4794459?site=stackoverflow').then(data => {
     const { reputation, badge_counts: { gold, silver, bronze }} = data.items[0];
-    database.child('stack').update({ reputation, gold, silver, bronze });
+    update('stack', { reputation, gold, silver, bronze });
   });
   get('https://api.github.com/users/OmarEinea').then(({followers, public_repos}) => {
-    database.child('github').update({ followers, repos: public_repos });
+    update('github', { followers, repos: public_repos });
   });
   get('https://api.github.com/users/OmarEinea/subscriptions').then(repos => {
-    database.child('github').update({ stars: repos.reduce((sum, repo) => sum + repo.stargazers_count, 0) });
+    update('github', { stars: repos.reduce((sum, repo) => sum + repo.stargazers_count, 0) });
   });
   get('https://github.com/users/OmarEinea/contributions', 'text').then(html => {
-    database.child('github').update({
+    update('github', {
       commits: Number(html.match(/<h2 class="f4.*">\s*(\d+)[\s\S]*<\/h2>/)[1]),
       graph: html.match(/<svg([\s\S]*?)<\/svg>/)[0]
         .replace('translate(16, 20)', 'translate(20, 24)')
@@ -35,15 +38,13 @@ app.get('/updateProfiles', (_, response) => {
     });
   });
   get('https://forum.xda-developers.com/member.php?u=4800636', 'text').then(html => {
-    database.child('xda').update({
+    update('xda', {
       posts: Number(html.match(/<li><span .*>Total Posts:<\/span>(.+)<\/li>/)[1].replace(',', '')),
       thanks: Number(html.match(/<li><span .*>Number of Thanks:<\/span>(.+)<\/li>/)[1])
     });
   });
-  get('https://forum.xda-developers.com/search.php?searchid=453872375&pp=1', 'text').then(html => {
-    database.child('xda').update({
-      threads: Number(html.match(/>Showing results \d+ to \d+ of (\d+)</)[1])
-    });
+  get('https://forum.xda-developers.com/search.php?do=finduser&u=4800636&starteronly=1', 'text').then(html => {
+    update('xda', { threads: Number(html.match(/>Showing results \d+ to \d+ of (\d+)</)[1]) });
   });
   response.send('Updating Profiles...');
 });
