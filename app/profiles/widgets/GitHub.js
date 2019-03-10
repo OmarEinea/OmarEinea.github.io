6878efcacd020760d71a4829648e45e2e28a4c43
Omar Einea <eineao@gmail.com>
2019-03-10 23:43:25 +0400

    Profiles: adopted GitHub graph to draw from stored integer

--- app/profiles/widgets/GitHub.js
+++ app/profiles/widgets/GitHub.js
@@ -3,6 +3,10 @@ import { Grid, Typography } from 'material-ui';
 import { logo } from 'db';
 import './GitHub.css';
 
+const colors = ['#eaeaea', '#c6e48b', '#7bc96f', '#239a3b', '#196127'], days = ['Mon', 'Wed', 'Fri'];
+let months = ['Jan', 'Feb', 'Mar', 'Apr', 'May', 'Jun', 'Jul', 'Aug', 'Sep', 'Oct', 'Nov', 'Dec'];
+months = months.splice(new Date().getMonth()).concat(months);
+
 export default class GitHub extends PureComponent {
   render() {
     const { repos, followers, stars, commits, graph } = this.props.data;
@@ -30,15 +34,27 @@ export default class GitHub extends PureComponent {
             <Typography variant="h5" align="center">
               {commits} contributions last year
             </Typography>
-            <div id="graph" dangerouslySetInnerHTML={{__html: graph}}/>
+            <div id="graph">
+              <svg width="669" height="108">
+                <g transform="translate(20, 24)">
+                  {months.map((month, i) => <text x={(4*i+1)*13} y="-10">{month}</text>)}
+                  {days.map((day, i) => <text dx="-20" dy={i*25+20}>{day}</text>)}
+                  {graph && [...Array(53)].map((_, week) =>
+                    <g transform={`translate(${week * 13}, 0)`}>
+                      {[...Array(7)].map((_, day) =>
+                        <rect width="10" height="10" x={13 - week} y={day * 12}
+                          fill={colors[graph.charAt(week * 7 + day)]}/>
+                      )}
+                    </g>
+                  )}
+                </g>
+              </svg>
+            </div>
             <Typography variant="caption" align="left" id="legend">
               Commits made by me
               <div style={{float: 'right'}}>
                 Less
-                <ul>
-                  {['#eee', '#c6e48b', '#7bc96f', '#239a3b', '#196127']
-                    .map(color => <li style={{backgroundColor: color}}></li>)}
-                </ul>
+                <ul>{colors.map(color => <li style={{backgroundColor: color}}></li>)}</ul>
                 More
               </div>
             </Typography>
