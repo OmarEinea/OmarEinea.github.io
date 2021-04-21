5f12d7b09361c2630a836cd4ea69c254c813dfa4
Omar Einea <eineao@gmail.com>
2021-04-22 01:01:56 +0400

    Deploy: fixed minification bug

--- minify.js
+++ minify.js
@@ -1,5 +1,5 @@
 const fs = require('fs'), file = 'app.min.js',
-  letters = 'abcdefghijklmnopqrstuvwyxz',
+  letters = 'opqrstuvwyxz',
   isFree = (letter, string) => !new RegExp(
     'function ' + letter + '\\(|function(?: [a-z0-9_]+)?\\((?:[a-z],)*?' + letter +
     '(?:,[a-z])*?\\)|var (?:[a-z][^,;]*?,)*?' + letter + '[^a-z:]'
@@ -12,10 +12,10 @@ const fs = require('fs'), file = 'app.min.js',
   };
 
 function* safeVar(string) {
-  for(let i = -1; i < letters.length; i++)
-    for(let j = 0; j < letters.length; j++) {
+  for (let i = -1; i < letters.length; i++)
+    for (let j = 0; j < letters.length; j++) {
       const letter = letters.charAt(i) + letters.charAt(j);
-      if(isFree(letter, string))
+      if (isFree(letter, string))
         yield letter;
     }
 }
@@ -40,5 +40,5 @@ fs.readFile(file, 'utf8', (_, js) => {
     js = `${newName}=${oldName}${index ? ',' : ';'}` + js.replace(new RegExp(oldName, 'g'), put(newName));
     console.log('Replaced', ctr, oldName);
   });
-  fs.writeFile(file, 'var ' + js, 'utf8', () => {});
+  fs.writeFile(file, 'var ' + js, 'utf8', () => { });
 });
