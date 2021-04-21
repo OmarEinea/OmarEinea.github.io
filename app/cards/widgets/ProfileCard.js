bf0d4c5c10ed7ff0f13ef533cc26d378f46896f0
Omar Einea <eineao@gmail.com>
2021-04-21 23:51:26 +0400

    Profiles: fixed text and logo overlapping issue

--- app/cards/widgets/ProfileCard.js
+++ app/cards/widgets/ProfileCard.js
@@ -5,32 +5,32 @@ import Gallery from 'gallery';
 import './Card.css';
 
 export default class ProfileCard extends PureComponent {
-  state = {image: false};
+  state = { image: false };
 
   render() {
-    const { name, images, children, use='', ...props } = this.props,
+    const { name, images, children, use = '', ...props } = this.props,
       { image } = this.state, [text, icon] = use.split(';');
     return (
       <Card class="card" {...props}>
-        <CardMedia style={{paddingTop: '64%', position: 'relative', backgroundPosition: 'left'}}
-          image={url(`profiles/${name}/preview.jpg`)} onClick={() => this.setState({image: true})}>
-          <Button class="image-button"><i/></Button>
+        <CardMedia style={{ paddingTop: '64%', position: 'relative', backgroundPosition: 'left' }}
+          image={url(`profiles/${name}/preview.jpg`)} onClick={() => this.setState({ image: true })}>
+          <Button class="image-button"><i /></Button>
           <i class="fas fa-images"
-            style={{position: 'absolute', bottom: 24, right: 24, fontSize: 20, color: '#757575'}}/>
+            style={{ position: 'absolute', bottom: 24, right: 24, fontSize: 20, color: '#757575' }} />
         </CardMedia>
         {image && <Gallery title={name} images={images} format="png"
-          folder="profiles" onClose={() => this.setState({image: false})}/>}
-        <div style={{padding: 8}}>
-          {children || <Grid container style={{padding: '16px 16px 12px'}}>
-            <Grid item md={7} xs={12}>
+          folder="profiles" onClose={() => this.setState({ image: false })} />}
+        <div style={{ padding: 8 }}>
+          {children || <Grid container direction="column" style={{ padding: '16px 16px 12px' }}>
+            <Grid item style={{ marginBottom: 8 }}>
               <a href={'my/' + name.toLowerCase().replace(/ /g, '')}>
-                <img height="42" src={profile(name)}/>
+                <img height="42" src={profile(name)} />
               </a>
             </Grid>
-            <Grid item md={5} xs={12}>
-              <Typography class="profile-use" variant="body1" style={{color: '#616161', lineHeight: 1.4}}>
-                <b style={{color: '#9e9e9e', fontSize: 14}}>Main Use:</b>
-                <br/><i class={'fa fa-fw fa-' + icon}/> {text}
+            <Grid item>
+              <Typography class="profile-use" variant="body1" style={{ color: '#616161', lineHeight: 1.4 }}>
+                <b style={{ color: '#9e9e9e', fontSize: 14 }}>Main Use:</b>
+                <br /><i class={'fa fa-fw fa-' + icon} /> {text}
               </Typography>
             </Grid>
           </Grid>}
